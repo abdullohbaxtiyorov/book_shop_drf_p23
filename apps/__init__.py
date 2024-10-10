a51d5f7149a2d022b9fcc4bd4ac2b69be05eaead
abdulloh <abdullohbaxtiyorov@gmail.com>
2024-10-10 22:33:20 +0500

    change

---
+++ apps/__init__.py
@@ -0,0 +1,3 @@
+from apps import users
+from apps import shops
+
---
+++ apps/shops/models.py
@@ -0,0 +1,24 @@
+from django.db.models import CharField, CASCADE, TextField, ImageField, Model, ForeignKey
+from mptt.models import MPTTModel, TreeForeignKey
+
+from apps.shared.models import TimeBasedModel
+from shared.models import SlugBasedModel
+
+
+class Section(TimeBasedModel):
+    name_image = ImageField(upload_to='shops/categories/name_image/%Y/%m/%d', null=True, blank=True)
+    intro = TextField(null=True, blank=True)
+    banner = ImageField(upload_to='shops/categories/banner/%Y/%m/%d', null=True, blank=True)
+
+
+class Category(MPTTModel):
+    name = CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', CASCADE, null=True, blank=True, related_name='subcategories')
+    section = ForeignKey('shops.Section', CASCADE, null=True, blank=True, related_name='categories')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+class Book(SlugBasedModel):
+    name = CharField(max_length=50, unique=True)
+    writer = CharField(max_length=50, unique=True)
